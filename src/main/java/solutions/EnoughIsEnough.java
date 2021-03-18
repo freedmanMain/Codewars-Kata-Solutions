124d2e5d806851ae29701a9288d13a88eff3cd3d
Maksim <max.freedman.main@gmail.com>
2021-03-19 00:27:20 +0200

    Created, solved and tested EnoughIsEnough class

---
+++ src/main/java/solutions/EnoughIsEnough.java
@@ -0,0 +1,17 @@
+package solutions;
+
+import java.util.LinkedList;
+
+public class EnoughIsEnough {
+    public static int[] deleteNth(int[] elements, int maxOccurrences) {
+    LinkedList<Integer> _list = new LinkedList<>();
+    for (int i = 0; i < elements.length; i++) {
+      int current = elements[i];
+      int countOfCurrent = (int)_list.stream().filter(x -> x == current).count();
+      if (countOfCurrent < maxOccurrences) {
+        _list.add(current);
+      }
+    }
+    return _list.stream().mapToInt(Integer :: intValue).toArray();
+  }
+}
