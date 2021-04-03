f3fb2e9d5661bc4c8f995d57c8b48c057a9bec5e
Maksim <max.freedman.main@gmail.com>
2021-04-03 19:54:35 +0300

    just commit

--- src/main/java/solutions/kuy7/ReverseNumber.java
+++ src/main/java/solutions/kuy7/ReverseNumber.java
@@ -3,7 +3,7 @@ package solutions.kuy7;
 public class ReverseNumber {
     public static int reverse(int number) {
         int result = Integer.parseInt(new StringBuilder().append(number)
-                    .reverse().toString().replace("-",""));
+                .reverse().toString().replace("-", ""));
         return number < 0 ? -result : result;
     }
 }
