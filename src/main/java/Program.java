3f8ae94be9d3065e868ca0203889fc70ee9dcd0e
Maksim <max.freedman.main@gmail.com>
2021-04-12 11:18:26 +0300

    solved circular list

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,12 @@
+import solutions.kuy7.CircularList;
+
 public class Program {
     public static void main(String[] args) {
+        CircularList<Integer> myList = new CircularList<>(1,2,3);
 
+        System.out.println(myList.next());
+        System.out.println(myList.next());
+        System.out.println(myList.next());
+        System.out.println(myList.next());
     }
 }
